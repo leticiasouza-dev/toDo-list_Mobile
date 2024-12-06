1ca1dbff60f4f156d18c78a96c973e38e6efdcfd
leticiaSouzaDev <lec667121@gmail.com>
2024-12-06 16:59:08 -0300

    commit inicial

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View} from "react-native";
+
+import { styles } from "./style";
+
+export function Home(){
+    return(
+        <View styles={styles.container}>
+            <View style={styles.header}>
+                    
+            </View>
+        </View>
+    )
+}
